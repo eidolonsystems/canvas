107e3ad376b02ae3fbfc339a599f71a2a291bee1
Sofiya Polyanska <sofiya@spiretrading.com>
2019-09-09 14:30:00 -0400

    Implemented node and edge.

--- source/edge.ts
+++ source/edge.ts
@@ -1,2 +1,34 @@
 export class Edge {
+  constructor(name: string, tail: Node, head: Node) {
+    this._name = name;
+    this._tail = tail;
+    this._head = head;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get head(): Node {
+    return this._head;
+  }
+
+  public get tail(): Node {
+    return this._tail;
+  }
+
+  public set name(newName: string) {
+    this._name = newName;
+  }
+
+  public set head(newHead: Node) {
+    this._head = newHead;
+  }
+  public set tail(newTail: Node) {
+    this._tail = newTail;
+  }
+
+  private _name: string;
+  private _tail: Node;
+  private _head: Node;
 }
